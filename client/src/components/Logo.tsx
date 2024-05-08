4c99dc5bc1ddd4cd482383039b9e4ec1f7aeaaf6
PavelKhomyakov <pakhom@fab.pasona.tech>
2024-05-08 13:27:58 +0900

    animation logo

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 import { List, ListMusic } from "lucide-react";
 import logo from "../assets/logo/myPSNA.mdb.svg";
+import "../utils/animations.css"
 
 const Logo = () => {
   return (
     <Link
       to="/"
-      className="hidden lg:flex justify-center items-center  rounded-lg mx-auto"
+      className=" justify-center items-center  rounded-lg mx-auto"
     >
-      <img src={logo} alt="logo" className="w-40" />
+      <div className="tracking-in-expand logo-font">MyPSNA.mdb</div>
     </Link>
   );
 };
