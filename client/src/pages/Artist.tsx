736250ff963d08bfd8fd3b212ec25aeafd6a1237
Ryan Esmores <eesmores@fab.pasona.tech>
2024-05-27 11:23:25 +0900

    Created New Componenent ArtistInfo, Made Artist Page Code Cleaner

--- client/src/pages/Artist.tsx
+++ client/src/pages/Artist.tsx
@@ -1,6 +1,7 @@
-import { type ArtistResponse, customFetch } from "@/utils";
+import { type ArtistResponse } from "@/utils";
 import { type LoaderFunction, useLoaderData } from "react-router-dom";
 import axios from "axios";
+import { ArtistInfo } from "@/components";
 
 const key = "DfDBmgofFwSMdzLNjRAL";
 const secret = "KXFRPyshHlGOKidtXcpeJPVpmcebZTUz";
@@ -19,80 +20,7 @@ export const loader: LoaderFunction = async ({ params }) => {
 
 const Artist = () => {
   const { data } = useLoaderData() as ArtistResponse;
-  console.log(data);
-  const {
-    name,
-    id,
-    images,
-    members,
-    profile,
-    releases_url,
-    resource_url,
-    aliases,
-    uri,
-    urls,
-  } = data;
-  return (
-    <div className="grid grid-cols-[auto,1fr,.4fr] gap-4 py-8 px-24">
-      <div className="w-32">
-        {images && <img src={images[0].uri} alt={name} />}
-      </div>
-      <div>
-        <h1 className="font-semibold">{name}</h1>
-        <table className="table-auto ">
-          <tbody>
-            <tr>
-              <th className="text-left pr-12 py-2 align-top ">Profile:</th>
-              <td className="text-left pr-12 py-2 align-top">
-                <p>{profile}</p>
-              </td>
-            </tr>
-            <tr>
-              <th className="text-left pr-12 py-2 align-top ">Sites:</th>
-              <td className="text-left pr-12 py-2 align-top">
-                <div className="flex flex-wrap">
-                  {urls &&
-                    urls.map((url, index) => {
-                      return (
-                        <div key={url} className="max-w-[700px]">
-                          <a
-                            key={url}
-                            href={url}
-                            className="text-blue-600 hover:underline break-words "
-                          >
-                            {url}
-                          </a>
-                          {index !== urls.length - 1 && <span>,&nbsp;</span>}
-                        </div>
-                      );
-                    })}
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th className="text-left pr-12 py-2 align-top">Aliases:</th>
-              <td className="text-left pr-12 py-2 align-top">
-                <div>
-                  {aliases &&
-                    aliases.map((alias, index) => {
-                      return <p key={index}>{alias.name}</p>;
-                    })}
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th className="text-left pr-12 py-2 align-top">Members:</th>
-              <td className="text-left pr-12 py-2 align-top">
-                {members &&
-                  members.map((member) => {
-                    return <p key={member.id}>{member.name}</p>;
-                  })}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+
+  return <ArtistInfo data={data} />;
 };
 export default Artist;
